bffa0ca64461412246c2dc44d168953629ed3c81
clockclock1 <santi163@163.com>
2025-08-26 12:40:44 +0800

    修改替换API地址

--- src/common.rs
+++ src/common.rs
@@ -1024,7 +1024,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "http://yc.clockclock1.top:21114".to_owned()
+    "http://47.92.28.8:1114".to_owned()
 }
 
 #[inline]
